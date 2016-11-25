f91de747d02ffc1d31b543186f4465abd1843458
Roman Nebesnuy <gadpro100@gmail.com>
2016-11-25 21:46:12 +0200

    fix to laravel 5.3

--- src/Pingpong/Admin/Seeders/PermissionsTableSeeder.php
+++ src/Pingpong/Admin/Seeders/PermissionsTableSeeder.php
@@ -28,7 +28,7 @@ class PermissionsTableSeeder extends Seeder
             ]);
         }
 
-        $permissions = Permission::lists('id')->toArray();
+        $permissions = Permission::pluck('id')->toArray();
 
         Role::find(1)->permissions()->attach($permissions);
     }
